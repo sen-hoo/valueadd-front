53406fd85fffd06473eda4a3d24a8482ef854389
sen.hu <sen.hu@zhaopin.com.cn>
2018-08-16 18:44:07 +0800

    add admin project

---
+++ valueadd-admin/src/router/index.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+
+Vue.use(Router)
+
+// export const constantRouterMap = [
+//   {
+//     path: '/login',
+//     component: () => import('@/views/login/index'),
+//     hidden: true
+//   }
+// ]
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    }
+  ]
+})
---
+++ valueadd-admin/src/views/login/index.vue
@@ -0,0 +1,4 @@
+<template>
+    <div class="login-container">
+    </div>
+</template>
