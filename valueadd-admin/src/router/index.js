ee5431552b55f20fd9c65352620f62e6a736532e
sen-hoo <125504460@qq.com>
2018-08-17 00:26:48 +0800

    add routers

--- valueadd-admin/src/router/index.js
+++ valueadd-admin/src/router/index.js
@@ -4,20 +4,32 @@ import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
-// export const constantRouterMap = [
-//   {
-//     path: '/login',
-//     component: () => import('@/views/login/index'),
-//     hidden: true
-//   }
-// ]
+export const constantRouterMap = [{
+    path: '/',
+    redirect: '/login'
+  },
+  {
+    path: '/login',
+    component: () =>
+      import ('@/views/login/index'),
+    hidden: true
+  }
+]
+
+// export default new Router({
+//   routes: [
+//     {
+//       path: '/',
+//       name: 'HelloWorld',
+//       component: HelloWorld
+//     }
+
+//   ]
+// })
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+  scrollBehavior: () => ({
+    y: 0
+  }),
+  routes: constantRouterMap
 })
--- valueadd-admin/src/views/login/index.vue
+++ valueadd-admin/src/views/login/index.vue
@@ -1,4 +1,11 @@
 <template>
     <div class="login-container">
+        <p>123411111111111111111111</p>
     </div>
 </template>
+
+<script>
+export default {
+    name: 'login'
+}
+</script>
