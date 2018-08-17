4e79ea3a15ab1a332e7ffa1816bd3baaae0f0903
sen.hu <sen.hu@zhaopin.com.cn>
2018-08-17 18:18:39 +0800

    add main page

---
+++ valueadd-admin/src/views/dashboard/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="dashboard-container">
+        <component :is="currentRole"></component>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "dashboard",
+    components: { adminDashboard, editorDashboard },
+    data() {
+        return {
+            currentRole: "adminDashboard"
+        };
+    },
+    computed: {
+        ...mapGetters(["roles"])
+    },
+    created() {
+        if (!this.roles.includes("admin")) {
+            this.currentRole = "editorDashboard";
+        }
+    }
+};
+</script>
+
---
+++ valueadd-admin/src/views/layout/Layout.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="app-wrapper" :class="classObj">
+    <div v-if="device==='mobile'&&sidebar.opened" class="drawer-bg" @click="handleClickOutside"></div>
+    <sidebar class="sidebar-container"></sidebar>
+    <div class="main-container">
+      <navbar></navbar>
+      <tags-view></tags-view>
+      <app-main></app-main>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'layout'
+}
+</script>
