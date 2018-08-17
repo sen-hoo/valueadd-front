57d39ccedcce64181584f6893b416c7bc2e90114
sen.hu <sen.hu@zhaopin.com.cn>
2018-08-17 17:51:43 +0800

    add login page

--- valueadd-admin/src/main.js
+++ valueadd-admin/src/main.js
@@ -1,19 +1,32 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import ElementUI from 'element-ui'
+import Element from 'element-ui'
+
+//css
+import 'element-ui/lib/theme-chalk/index.css'
+import '@/styles/index.scss'
 
 import App from './App'
 import router from './router'
+import store from './store'
+
+import i18n from './lang' // 引入多国语言包
+import './icons' // 引入icon组件封装
 
 Vue.config.productionTip = false
 
-Vue.use(ElementUI, { size: 'medium' })
+Vue.use(Element, {
+  size: 'medium',
+  zIndex: 3000,
+  i18n: (key, value) => i18n.t(key, value)
+})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
+  store,
+  i18n,
+  render: h => h(App)
 })
