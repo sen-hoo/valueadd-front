4de7ac319775eafab63a0249c7508e02b2746705
sen-hoo <125504460@qq.com>
2018-08-28 23:04:08 +0800

    add something

--- valueadd-admin/src/router/index.js
+++ valueadd-admin/src/router/index.js
@@ -1,47 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+//import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
-/* Layout */
-import Layout from '@/views/layout/Layout'
-
 export const constantRouterMap = [{
+    path: '/',
+    redirect: '/login'
+  },
+  {
     path: '/login',
     component: () =>
       import ('@/views/login/index'),
     hidden: true
-  },
-  {
-    path: '/401',
-    component: () =>
-      import ('@/views/errorPage/401'),
-    hidden: true
-  },
-  {
-    path: '/404',
-    component: () =>
-      import ('@/views/errorPage/404'),
-    hidden: true
-  },
-  {
-    path: '',
-    redirect: '/login'
   }
-  // {
-  //   path: '',
-  //   component: Layout,
-  //   redirect: 'dashboard',
-  //   children: [
-  //     {
-  //       path: 'dashboard',
-  //       component: () => import('@/views/dashboard/index'),
-  //       name: 'dashboard',
-  //       meta: { title: 'dashboard', icon: 'dashboard', noCache: true }
-  //     }
-  //   ]
-  // },
 ]
 
 // export default new Router({
@@ -61,35 +33,3 @@ export default new Router({
   }),
   routes: constantRouterMap
 })
-
-export const asyncRouterMap = [{
-  path: '/permission',
-  component: Layout,
-  redirect: '/permission/index',
-  alwaysShow: true, // will always show the root menu
-  meta: {
-    title: 'permission',
-    icon: 'lock',
-    roles: ['admin', 'editor'] // you can set roles in root nav
-  },
-  children: [{
-      path: 'page',
-      component: () =>
-        import ('@/views/permission/page'),
-      name: 'pagePermission',
-      meta: {
-        title: 'pagePermission',
-        roles: ['admin'] // or you can only set roles in sub nav
-      }
-    },
-    {
-      path: 'directive',
-      component: () =>
-        import ('@/views/permission/directive'),
-      name: 'directivePermission',
-      meta: {
-        title: 'directivePermission'
-        // if do not set roles, means: this page does not require permission
-      }
-    }]
-}]
