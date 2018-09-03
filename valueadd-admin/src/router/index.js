f6ed37bca0cde1d3bbee773ea6625de1c10ebe75
sen-hoo <125504460@qq.com>
2018-09-03 08:46:13 +0800

    add fomat lines

--- valueadd-admin/src/router/index.js
+++ valueadd-admin/src/router/index.js
@@ -7,34 +7,39 @@ Vue.use(Router);
 /* Layout */
 import Layout from "@/views/layout/Layout";
 
-export const constantRouterMap = [
-  {
+export const constantRouterMap = [{
     path: "/login",
-    component: () => import("@/views/login/index"),
+    component: () =>
+      import("@/views/login/index"),
     hidden: true
   },
   {
     path: "/401",
-    component: () => import("@/views/errorPage/401"),
+    component: () =>
+      import("@/views/errorPage/401"),
     hidden: true
   },
   {
     path: "/404",
-    component: () => import("@/views/errorPage/404"),
+    component: () =>
+      import("@/views/errorPage/404"),
     hidden: true
   },
   {
     path: '',
     component: Layout,
     redirect: 'dashboard',
-    children: [
-      {
-        path: 'dashboard',
-        component: () => import('@/views/dashboard/index'),
-        name: 'dashboard',
-        meta: { title: 'dashboard', icon: 'dashboard', noCache: true }
+    children: [{
+      path: 'dashboard',
+      component: () =>
+        import('@/views/dashboard/index'),
+      name: 'dashboard',
+      meta: {
+        title: 'dashboard',
+        icon: 'dashboard',
+        noCache: true
       }
-    ]
+    }]
   }
 ];
 
@@ -45,36 +50,35 @@ export default new Router({
   routes: constantRouterMap
 });
 
-export const asyncRouterMap = [
-  {
-    path: "/permission",
-    component: Layout,
-    redirect: "/permission/index",
-    alwaysShow: true, // will always show the root menu
-    meta: {
-      title: "permission",
-      icon: "lock",
-      roles: ["admin", "editor"] // you can set roles in root nav
-    },
-    children: [
-      {
-        path: "page",
-        component: () => import("@/views/permission/page"),
-        name: "pagePermission",
-        meta: {
-          title: "pagePermission",
-          roles: ["admin"] // or you can only set roles in sub nav
-        }
-      },
-      {
-        path: "directive",
-        component: () => import("@/views/permission/directive"),
-        name: "directivePermission",
-        meta: {
-          title: "directivePermission"
-          // if do not set roles, means: this page does not require permission
-        }
+export const asyncRouterMap = [{
+  path: "/permission",
+  component: Layout,
+  redirect: "/permission/index",
+  alwaysShow: true, // will always show the root menu
+  meta: {
+    title: "permission",
+    icon: "lock",
+    roles: ["admin", "editor"] // you can set roles in root nav
+  },
+  children: [{
+      path: "page",
+      component: () =>
+        import("@/views/permission/page"),
+      name: "pagePermission",
+      meta: {
+        title: "pagePermission",
+        roles: ["admin"] // or you can only set roles in sub nav
       }
-    ]
-  }
-];
+    },
+    {
+      path: "directive",
+      component: () =>
+        import("@/views/permission/directive"),
+      name: "directivePermission",
+      meta: {
+        title: "directivePermission"
+        // if do not set roles, means: this page does not require permission
+      }
+    }
+  ]
+}];
